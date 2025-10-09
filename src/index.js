1bc4689e46ba3a0de7c025458a222a6d1a910da7
mkmelhundley-code <mkmelhundley@gmail.com>
2025-10-09 14:53:41 -0700

    Built basic search engine (no API)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-alert("hello world");
+function searchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", searchSubmit);
